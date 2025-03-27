b76edd1e167920325299b0744894b7092bfc5bf2
Zyorel <dftorresr.1@gmail.com>
2025-03-26 19:20:21 -0500

    microservice

--- Microservices/auth-microservice/src/main.ts
+++ Microservices/auth-microservice/src/main.ts
@@ -1,19 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   try {
-    const app = await NestFactory.create(AppModule);
-    app.enableCors({
-      origin: process.env.FRONTEND_URL,
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true
-    })
-    await app.listen(process.env.PORT ?? 3000);
-    console.log(`Application running on: ${await app.getUrl()}`)
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+      AppModule,
+      {
+        transport: Transport.NATS,
+        options: {
+          host: process.env.BACKEND_URL,
+          port: process.env.PORT ?? 3000,
+        },
+      },
+    );
+    // app.enableCors({
+    //   origin: process.env.FRONTEND_URL,
+    //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    //   credentials: true,
+    // });
+    await app.listen();
+    console.log(`Application running on: ${await 30}`);
   } catch (error) {
     console.error('Error during application bootstrap: ', error);
-    process.exit(1)
+    process.exit(1);
   }
 }
 
